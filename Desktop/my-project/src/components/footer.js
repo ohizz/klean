3faf5ddc9d709d5d8dec3fa9d3c6a0f06f892e68
ohis <jtsamuel4312@gmail.com>
2022-07-11 21:17:52 +0100

    new todoapp

---
+++ Desktop/my-project/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+ return(
+  <footer>
+   <p>created by <a className="ohis" href="ohis.netlify.app" target="_blank" rel='noopener noreferrer'>ohis</a></p>
+  </footer>
+ )
+}
+
+export default Footer;
