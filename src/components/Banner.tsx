d993dbfded28ed6416ea73641e9ec7c4a29aa996
ohizz <jtsamuel3084@gmail.com>
2023-08-08 19:46:05 +0100

    newthisss

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ import {SiCnn} from "react-icons/si"
 
 const Banner = () => {
  return( 
-    <div className='mt-10 flex flex-wrap flex-row md:flex-row md:justify-between justify-center item-center max-w-[1100px] my-0 mx-auto'>
+    <div className='mt-10 mx-4 md:mx-0 flex flex-wrap flex-row md:flex-row md:justify-between justify-center item-center max-w-[1100px] my-0 mx-auto'>
 <div className='flex mb-14 lg:mb-0 flex-col text-center w-2/2 lg:w-1/2'>
         <h1 className='text-zinc-900 text-7xl  md:text-7xl leading-tighter pb-8 font-black'>
             Your <span className='text-blue-600'>cleaning service</span> for everything cleaning
--- src/components/Whatwedo.tsx
+++ src/components/Whatwedo.tsx
@@ -4,8 +4,11 @@ const Whatwedo = () => {
  return(
     <div className=" flex">
         <div>
+            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.thespruce.com%2Fthmb%2FbUWrSS8as3ACgC96Tm4OEDzin9g%3D%2F1500x0%2Ffilters%3Ano_upscale()%3Amax_bytes(150000)%3Astrip_icc()%2FBestCouchCleaningServices_edit-4919045a0e6842d9baa7c45dec5f5c4d.jpg&tbnid=pI5f5BUqTeuiqM&vet=10CLcBEDMoogJqFwoTCMigzumIzYADFQAAAAAdAAAAABAD..i&imgrefurl=https%3A%2F%2Fwww.thespruce.com%2Fbest-couch-cleaning-services-5094435&docid=skOiIXw6u1bWDM&w=1500&h=1000&q=cleaning%20services&ved=0CLcBEDMoogJqFwoTCMigzumIzYADFQAAAAAdAAAAABAD"/>
+        <div>
         <h1>residential service</h1>
         <p>we do resdiential cleaning service a</p>
+        </div>
         </div>
         <div>
             <h1>commercial and office service</h1>
