3be839bdff06cba83768f0a665cda9ca44cedce7
ohizz <jtsamuel3084@gmail.com>
2023-08-08 20:16:10 +0100

    meee

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
  return(
     <div className=" flex justify-between items-center border-b border-zinc-200 pt-8 pb-4 px-6">
-<h1 className="text-blue-600 text-3xl font-black tracking-tight">klean.</h1>
+<h1 className="text-blue-600 text-2xl font-black tracking-tight">klean.</h1>
 <a href="tel:123-456-7890" className="text-zinc-100 bg-zinc-900 border p-3 rounded-full ">Book a service</a>
     </div>
  )
