4ea11babc009a21fe329ae265ae78edac3f1dbf5
ohizz <jtsamuel3084@gmail.com>
2023-08-11 15:30:14 +0100

    llll

--- src/components/Why.tsx
+++ src/components/Why.tsx
@@ -5,10 +5,10 @@ const Why = () => {
     <div className="my-20 px-4  flex flex-col md:flex-row justify-between flex-wrap">
 <div className="flex flex-col w-full md:w-1/2">
         <h2 className="text-6xl">What do we do</h2>
-        <p className="font-sans text-slate-600 text-md leading-6">Our jobs is to clean your home, office as well as you do.</p>
+        <p className="font-sans text-slate-600 text-md leading-tight tracking-tighter">Our jobs is to clean your home, office as well as you do.</p>
     </div>
     <div className=" mt-8 md:mt-0 w-full md:w-2/5">
-        <p className="font-sans text-slate-800 text-lg">if your job make you busy for home chore and you get tired of cleaning your house then klean is with you. You dont have worry about cleaning anymore we know your home as well</p>
+        <p className="font-sans text-slate-800 text-lg leading-tight tracking-tighter">if your job make you busy for home chore and you get tired of cleaning your house then klean is with you. You dont have worry about cleaning anymore we know your home as well</p>
     </div>
     </div>
     
