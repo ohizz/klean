16035a2be17d8a2b2fc3399b222cd8c7b5f769bf
ohizz <jtsamuel3084@gmail.com>
2023-08-11 15:22:27 +0100

    kk

--- src/components/Why.tsx
+++ src/components/Why.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Why = () => {
  return(
-    <div className="my-20 px-4 md:px-20 flex flex-col md:flex-row justify-between flex-wrap">
+    <div className="my-20 px-4  flex flex-col md:flex-row justify-between flex-wrap">
 <div className="flex flex-col w-full md:w-1/2">
         <h2 className="text-6xl">What do we do</h2>
-        <p className="text-slate-800">Our jobs is to clean your home, office as well as you do.</p>
+        <p className="font-sans text-slate-600 text-md leading-6">Our jobs is to clean your home, office as well as you do.</p>
     </div>
-    <div className="w-full md:w-2/5">
-        <p className=" text-slate-800">if your job make you busy for home chore and you get tired of cleaning your house then klean is with you. You dont have worry about cleaning anymore we know your home as well</p>
+    <div className=" mt-8 md:mt-0 w-full md:w-2/5">
+        <p className="font-sans text-slate-800 text-lg">if your job make you busy for home chore and you get tired of cleaning your house then klean is with you. You dont have worry about cleaning anymore we know your home as well</p>
     </div>
     </div>
     
